f6d123a30be762fee97eeb5bf2041193a02ce86f
Abid M N <abid@hewanesia.id>
2025-12-16 11:25:29 +0700

    update sw.js 2

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,16 @@
 import './bootstrap';
 import './audio-player';
-
 import './password-toggle';
+
+// Service Worker Registration
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(registration => {
+        console.log('SW registered:', registration);
+      })
+      .catch(error => {
+        console.log('SW registration failed:', error);
+      });
+  });
+}
